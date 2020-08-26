3a8bfe80bd58cd95bbdadcb66949d68945c99162
= <tbytnar@gmail.com>
2020-08-26 20:11:39 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+
+const express = require('express')
+const app = express()
+
+app.listen(3000, function() {
+	console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
