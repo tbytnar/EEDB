0eef6d897e9fa5e9630b59cd0807660e7cb9a600
Tim Bytnar <tbytnar@gmail.com>
2020-08-26 18:58:48 -0500

    Fixed bad git cache

---
+++ public/main.js
@@ -0,0 +1,14 @@
+app.use(express.static('public'))
+
+const update = document.querySelector('#update-button')
+
+update.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Darth Vadar',
+            quote: 'I find your lack of faith disturbing.'
+          })
+    })
+})
--- server.js
+++ server.js
@@ -1,12 +1,57 @@
-// server.js
-
+// Express
 const express = require('express')
 const app = express()
 
-app.listen(3000, function() {
-	console.log('listening on 3000')
+// Body parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+
+// Firebase
+const admin = require('firebase-admin');
+const serviceAccount = require('../workshop-bot-286003-597bb10efe90.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+const quotes = db.collection('quotes');
+
+// Handlers
+app.get('/', async (req, res) => {
+	let data = []
+	await quotes.get().then(querySnapshot => {
+		let docs = querySnapshot.docs;
+		for (let doc of docs) {
+			const selectedItem = {
+				id: doc.id,
+				name: doc.data().name,
+				quote: doc.data().quote
+			};
+			data.push(selectedItem);
+		}
+	})
+	.then( response => {
+		res.render('index.ejs', { quotes: data });
+	})
+	.catch(error => console.error(error));
 })
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+app.post('/quotes', async (req, res) => {
+	await quotes.add(req.body)
+	.then(result => {
+		res.redirect('/');
+	})
+	.catch(error => console.error(error));
 })
+
+app.put('/quotes', (req, res) => {
+	console.log(req.body)
+  })
+
+// App listner
+app.listen(3000, function() {
+	console.log('listening on 3000')
+}) 
